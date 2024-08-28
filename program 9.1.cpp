ce9adf86030a579ff1e283a50ca956cff7921940
MahreenArif43 <mahreenarif06@gmail.com>
2024-08-28 17:54:01 +0500

    Add files via upload

---
+++ program 9.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Function declaration
+void displayMessage();
+
+int main() {
+    // Call the function to display the message
+    displayMessage();
+    return 0;
+}
+
+// Function definition
+void displayMessage() {
+    std::cout << "Programming makes life interesting" << std::endl;
+}
